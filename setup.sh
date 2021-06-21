ca7318bd488561f72f1fd8e82c5d99329a562051
metalex9 <alexbainter@gmail.com>
2021-06-21 18:10:31 -0500

    Install vim-plug

--- setup.sh
+++ setup.sh
@@ -65,6 +65,8 @@ installIfMissing vim
 
 # neovim
 installIfMissing nvim neovim
+sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
+       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
 
 # git
 installIfMissing git git-all
